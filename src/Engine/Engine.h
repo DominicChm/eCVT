9d44b08247ad197f498108c9c240caaa8e2c1e4b
ShainaBagri <shainabagri@gmail.com>
2020-12-29 18:12:24 -0800

    Create Engine class

---
+++ src/Engine/Engine.h
@@ -0,0 +1,24 @@
+#ifndef Engine_h
+#define Engine_h
+
+#include <Arduino.h>
+#include "ControlLoop.h"
+// #include "LookupTables.h"
+
+class Engine: public ControlLoop {
+   public:
+      enum State {INITIALIZE, DISENGAGED, ENGAGED_REST, ENGAGED_UPDATEPID};
+
+      Engine(FSMVars fsm, PIDController pid);
+
+      void run();
+      int8_t getState();
+
+   private:
+      State state;
+      void ctrlISR();
+      int32_t pRatioToCounts(int16_t ratio);
+      int32_t sRatioToCounts(int16_t ratio);
+};
+
+#endif
