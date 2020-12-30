a619eb7befd8022be823b841d5f010f157eebe1e
ShainaBagri <shainabagri@gmail.com>
2020-12-29 16:30:45 -0800

    Create ControlLoop class

---
+++ lib/ControlLoop/ControlLoop.h
@@ -0,0 +1,19 @@
+#ifndef ControlLoop_h
+#define ControlLoop_h
+
+#include "Task.h"
+#include "PIDController.h"
+
+class ControlLoop: public Task {
+   public:
+      ControlLoop(FSMVars fsm, PIDController pid);
+
+      virtual void run() = 0;
+      virtual int8_t getState() = 0;
+      PIDController getPID();
+
+   protected:
+      PIDController pid;      // Ratio Percent / Revolutions per Minute (%/RPM)
+};
+
+#endif
