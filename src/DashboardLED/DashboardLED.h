572dc318063194dc7d8b466bf40aba9f20435530
ShainaBagri <shainabagri@gmail.com>
2021-01-06 20:12:34 -0800

    Create DashboardLED class

---
+++ src/DashboardLED/DashboardLED.h
@@ -0,0 +1,23 @@
+#ifndef DashboardLED_h
+#define DashboardLED_h
+
+#include "Task.h"
+
+class DashboardLED: public Task {
+    public:
+        enum State {INITIALIZE, HUB_STATE, BKSHIFT_ON, UPSHIFT_ON, BOTH_LEDS_ON, FLASH_BKSHIFT, FLASH_UPSHIFT};
+
+        DashboardLED(FSMVars fsm, int8_t UPSHIFT_LED, int8_t BKSHIFT_LED);
+
+        void run();
+    
+    private:
+        State state;
+        int16_t prevRatio;      // Ratio Percent (%)
+        int16_t currRatio;      // Ratio Percent (%)
+        uint32_t prevTime;      // Milliseconds (ms)
+        int8_t UPSHIFT_LED;
+        int8_t BKSHIFT_LED;
+};
+
+#endif
