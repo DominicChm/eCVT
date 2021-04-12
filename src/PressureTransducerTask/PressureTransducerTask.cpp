79e7dd80f575190b1147d32762fa7270d7c9ac59
Rahul Goyal <rahulxgoyal@gmail.com>
2021-04-11 20:49:09 -0700

    Create pressure transducer task

---
+++ src/PressureTransducerTask/PressureTransducerTask.cpp
@@ -0,0 +1,20 @@
+#include "PressureTransducerTask.h"
+
+
+PressureTransducerTask::PressureTransducerTask(
+    FSMVars fsm, BrakePressure fBrakePressure, BrakePressure rBrakePressure):
+    Task (fsm), fBrakePressure(fBrakePressure), rBrakePressure(rBrakePressure) { }
+
+
+void PressureTransducerTask::run() {
+    switch(state) {
+        case INITIALIZE:
+            state = UPDATE;
+            return;
+        
+        case UPDATE:
+            fsm.fBrakePressure = fBrakePressure.read();
+            fsm.rBrakePressure = rBrakePressure.read();
+            return;
+    }
+}
---
+++ src/PressureTransducerTask/PressureTransducerTask.h
@@ -0,0 +1,26 @@
+#ifndef PressureTransducer_h
+#define PressureTransducer_h
+
+#include "./Task/Task.h"
+#include "BrakePressure.h"
+
+class PressureTransducerTask: public Task {
+    public:
+        enum State {
+            INITIALIZE,
+            UPDATE};
+        
+        PressureTransducerTask(
+            FSMVars fsm,
+            BrakePressure fBrakePressure,
+            BrakePressure rBrakePressure);
+        
+        void run();
+    
+    private:
+        State state;
+        BrakePressure fBrakePressure;
+        BrakePressure rBrakePressure;
+};
+
+#endif
