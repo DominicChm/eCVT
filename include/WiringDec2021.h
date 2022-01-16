078552877110f16740ee4dddac8e674263bce044
ShainaBagri <shainabagri@gmail.com>
2022-01-15 17:23:32 -0800

    Add WiringDec2021.h

---
+++ include/WiringDec2021.h
@@ -0,0 +1,51 @@
+/*
+ *  WiringDec2019.h - DAQ and eCVT wiring configuration.
+ *  Created by Rahul Goyal, December 2019.
+ *  Released to Cal Poly Baja SAE. ;)
+ */
+
+#ifndef WiringDec2021_h
+#define WiringDec2021_h
+
+#include <Arduino.h>
+
+// Hall Effect Sensors
+const int8_t ENGINE_SPEED_PIN = 5;
+const int8_t RWHEELS_SPEED_PIN = 6;
+
+// Encoders
+/* Swap A and B pins to swap direction. */
+const int8_t P_ENC_A = 24;
+const int8_t P_ENC_B = 25;
+const int8_t S_ENC_A = 26;
+const int8_t S_ENC_B = 27;
+
+// Load Cells
+const int8_t P_LC_SCK = 13;
+const int8_t P_LC_SDA = 28;
+const int8_t S_LC_SCK = 14;
+const int8_t S_LC_SDA = 15;
+
+// Motors
+/* Swap A and B pins to swap direction. */
+const int8_t P_MOT_INA = 36;
+const int8_t P_MOT_INB = 37;
+const int8_t P_MOT_PWM = 22;
+const int8_t S_MOT_INA = 20;
+const int8_t S_MOT_INB = 21;
+const int8_t S_MOT_PWM = 23;
+
+// Pressure Transducers
+// const int8_t FBRAKE_PRESSURE = 34;
+// const int8_t RBRAKE_PRESSURE = 33;
+
+// Dashboard
+const int8_t LAUNCH_BUTTON = 3;
+const int8_t ECVT_STATUS_LED = 2;
+const int8_t UPSHIFT_LED = 0;
+const int8_t BKSHIFT_LED = 1;
+
+// Communication
+#define DAQ_ECVT_SERIAL Serial2
+
+#endif
